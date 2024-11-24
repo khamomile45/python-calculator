b3d5ccf1d0b666bb1c74d97d224393a8cd931733
khamomile45 <109598575+khamomile45@users.noreply.github.com>
2024-11-24 10:25:27 +0000

    6588197_testcase

--- calculator.py
+++ calculator.py
@@ -3,23 +3,51 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        if a>=0 and b>=0:
+            for i in range(b):
+                result = self.add(result, a)
+        elif a>=0 and b<0 :
+            for i in range(a):
+                result = self.add(result, b)
+        elif a<0 and b<0:
+            for i in range(abs(a)):
+                result = self.add(result, abs(b))
+       
         return result
-
+    
     def divide(self, a, b):
         result = 0
-        while a > b:
+        while a >= b and a>0 and b>0:
             a = self.subtract(a, b)
-            result += 1
+            result +=1
+        while a >= b and a>0 and b<0:
+            a = self.add(a, b)
+            result -=1
+        while a < b and a>0 and b>0:
+            a = self.add(a, b)
+            result
+        while a < b and a<0 and b>0:
+            a = self.add(a, b)
+            result -=1
+        while a>=b and a<0 and b<0:
+            b =self.subtract(b,a)
+            result +=1
+        while a <= b and a<0 and b<0:
+            a = self.subtract(a, b)
+            result +=1
+        while a <= b and a<0 and b>0:
+            a = self.add(a, b)
+            result +=1
+            if result+b >0:
+                result +=1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
@@ -28,7 +56,7 @@ if __name__ == "__main__":
     calc = Calculator()
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
-    print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: subtraction: ", calc.subtract(-4, 6))
+    print("Example: multiplication: ", calc.multiply(-6, -4))
+    print("Example: division: ", calc.divide(1, -10))
+    print("Example: modulo: ", calc.modulo(9, 10))
--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,60 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+    def test_add(self):
+        self.assertEqual(self.calc.add(-2,-5),-7)
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(-2,5),3)
+    
+    def test_add(self):
+        self.assertEqual(self.calc.add(2,-5),-3)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4,-2),6)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(-4,-4),0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4,-2),-8)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(-4,-2),8)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4,2),8)
+
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10,2), 5)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10,-2), -5)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(0,0), 0)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(-10,-20), 2)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(-10,-2), 5)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10,3), 3)
+    
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10,5), 0)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10,3), 1)
+    
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(1,10), 1)
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
